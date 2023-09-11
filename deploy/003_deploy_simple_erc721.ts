82e6db9da82669967de1a85c8c9f1dc18d78e99b
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-09-11 12:30:42 -0400

    Functional deployment to sepolia testnet

--- deploy/003_deploy_simple_erc721.ts
+++ deploy/003_deploy_simple_erc721.ts
@@ -1,12 +1,7 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-// import * as dotenv from 'dotenv';
-// dotenv.config({path: __dirname + '/.env'});
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-	// const {deployments} = hre;
-	// const {deploy} = deployments;
-	// const deployer: any = process.env.PRIVATE_KEY;
 	const {deployer} = await hre.getNamedAccounts();
 	const {deploy} = hre.deployments;
 	const useProxy = !hre.network.live;
