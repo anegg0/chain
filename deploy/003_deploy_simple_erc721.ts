39b8ede1f21155a47682d237dd79baf0f21ec5e1
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-09-11 11:57:44 -0400

    Functional nft compilation & deployment>localhost

---
+++ deploy/003_deploy_simple_erc721.ts
@@ -0,0 +1,24 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+// import * as dotenv from 'dotenv';
+// dotenv.config({path: __dirname + '/.env'});
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	// const {deployments} = hre;
+	// const {deploy} = deployments;
+	// const deployer: any = process.env.PRIVATE_KEY;
+	const {deployer} = await hre.getNamedAccounts();
+	const {deploy} = hre.deployments;
+	const useProxy = !hre.network.live;
+	await deploy('NFT', {
+		from: deployer,
+		args: [],
+		log: true,
+		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+	});
+
+	return !useProxy; // when live network, record the script as executed to prevent rexecution
+};
+export default func;
+func.id = 'deploy_simple_erc721'; // id required to prevent reexecution
+func.tags = ['NFT'];
